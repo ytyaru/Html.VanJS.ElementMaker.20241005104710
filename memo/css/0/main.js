54b4d75990ebfdf5a617d298063151917430d5af
ytyaru <yttry0@gmail.com>
2024-10-07 19:49:36 +0900

    add memo

---
+++ memo/css/0/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', async(e)=>{
+    console.log(document.styleSheets)
+});
+window.addEventListener('beforeunload', async(e)=>{
+    console.log('終了')
+});
+
